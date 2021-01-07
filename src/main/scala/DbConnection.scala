0338e2237c79ebe1a3af5aa9eadb9149aabc3d30
tugay <tugay.celmeli.7@gmail.com>
2021-01-07 16:22:58 +0300

    Added IF NOT EXISTS to create table sql

--- src/main/scala/DbConnection.scala
+++ src/main/scala/DbConnection.scala
@@ -19,7 +19,7 @@ object PostgreSqlScala extends App{
 
     val statement = connection.createStatement()
 
-    var createAccountTable = "CREATE TABLE accounts " +
+    var createAccountTable = "CREATE TABLE IF NOT EXISTS accounts " +
     "(user_id serial PRIMARY KEY, username VARCHAR ( 50 ) UNIQUE NOT NULL," +
     "password VARCHAR ( 50 ) NOT NULL, email VARCHAR ( 255 ) UNIQUE NOT NULL);"
 
